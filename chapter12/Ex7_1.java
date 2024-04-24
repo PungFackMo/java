306a5492f891179e0eac44cbd548e5f485e40840
PungFackMo <165879434+PungFackMo@users.noreply.github.com>
2024-04-24 11:33:49 +0900

    상속 1

---
+++ chapter12/Ex7_1.java
@@ -0,0 +1,13 @@
+package chapter12;
+
+public class Ex7_1 {
+
+	public static void main(String[] args) {
+		SmartTv stv=new SmartTv();		//인스턴스
+		stv.channelUp();
+		stv.channelDown();
+		stv.displayCaption("Hellow World!");
+
+	}
+
+}
