5b3dea093cc0b51b7a7a23fea682409d252bb36b
PungFackMo <165879434+PungFackMo@users.noreply.github.com>
2024-04-24 11:50:58 +0900

    예외 1

---
+++ chapter18/ClassCastException.java
@@ -0,0 +1,15 @@
+package chapter18;
+
+public class ClassCastException {
+	
+	public static void main(String[] args) {
+		//타입 변환은 상위 클래스와 하위 클래스하고
+		//구현 클래스와 인터페이스 간에도 발생합니다.
+		Animal animal1=new Dog();
+		Animal animal2=new Animal();
+		Dog dog1=(Dog)animal1;
+		Dog dog2=(Dog)animal2;
+		
+	}
+
+}
